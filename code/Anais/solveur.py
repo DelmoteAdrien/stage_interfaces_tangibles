cc77f301df20fd5fc3ce97fcb83565f9879891eb
Anais CLOTILDE <anais.clotilde@irit.fr>
2024-05-28 10:26:01 +0200

    ajout d'un chronometrage du temps de recherche des solutions (pas d'optimisation pour l'instant)

--- code/Anais/solveur.py
+++ code/Anais/solveur.py
@@ -1,4 +1,5 @@
 from constraint import * #importation de la librairie python-constraint
+import time
 
 """
 Rappel de l'exercice 3 du TD d'Algorithmique Avancée sur les problèmes de résolution de contraintes :
@@ -17,7 +18,10 @@ problem.addVariables("D", range(7)) #initialisation de la quatrième tâche
 
 problem.addConstraint(lambda C, D: C + 3 <= D, ("C","D")) #la tâche 3 (de durée 3) doit se faire avant la tâche 4
 problem.addConstraint(lambda B, C: (B <= C - 1)|(C + 3 <= B), ("B","C")) #les tâches 2 (de durée 1) et 3 (de durée 3) ne peuvent se faire en parallèle
+debut = time.perf_counter() #debut de la recherche de solutions
 solutions = problem.getSolutions() #résolution du problème
+fin = time.perf_counter() #fin de la recherche de solutions
+print("Durée de la recherche de solutions :",(fin-debut),"s\n") #affichage de la durée de la recherche de solutions
 for i in range(len(solutions)): #affichage des solutions
     print("Solution n°",i+1," :") #affichage du numéro de la solution
     print("debut de la tache A :", solutions[i]["A"]) #affichage du début de la tâche A dans cette solution
